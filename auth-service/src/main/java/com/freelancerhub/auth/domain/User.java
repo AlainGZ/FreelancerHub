b8b954c559762cacc9353b35f27dfd1d322c95ba
AlainGZ <devalgz955@gmail.com>
2025-05-15 14:18:51 -0500

    Se crea la clase usuario

---
+++ auth-service/src/main/java/com/freelancerhub/auth/domain/User.java
@@ -0,0 +1,22 @@
+package com.freelancerhub.auth.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	@Id
+	private String id;
+
+	private String username;
+	private String email;
+	private String password;
+	private Set<String> roles;
+}
